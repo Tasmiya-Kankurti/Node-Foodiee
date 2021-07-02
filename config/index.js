2b47bd5a5146c56483127b6cb2a4740263035f66
Tasmiya-Kankurti <tasmiyakankurti@gmail.com>
2021-07-02 22:46:37 +0530

    Mongo Database created

--- config/index.js
+++ config/index.js
@@ -2,5 +2,6 @@ require('dotenv/config')
 module.exports = {
     serviceID : process.env.serviceID,
     accountSID : process.env.accountSID,
-    authToken : process.env.authToken
+    authToken : process.env.authToken,
+    MONGO_URL : process.env.mongo_URL
 }
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const express = require("express")
-const app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const MONGO_URL = require('./config/').MONGO_URL
 
 const phoneAuth = require('./src/routes/phoneAuthRoute')
 
-const port = 3000
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false, 
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Database Successfully connected :)")
+}).catch((error)=>{
+    console.log(`ERROR: ${error.message}`);
+})
+
+const PORT =  process.env.PORT || 3000
 
 app.get('/',(req,res) => {
     res.send("Test")
@@ -11,8 +28,6 @@ app.get('/',(req,res) => {
 
 app.use('/api/phoneauth', phoneAuth)
 
-app.listen(port, () => {
-    console.log(`Server is running at ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`)
 })
-
-// LtEzfvT5_vTJIf2Bnl72B5lTtGHpsQgKH_YZYti5
